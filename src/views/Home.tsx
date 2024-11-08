c8e0d7faa726a679932b93e605d6c42a11b0bec9
Samuel Hermany <samuelhermany@hotmail.com>
2024-11-07 22:56:13 -0300

    Context (#5)
    * SnackBar e Alert criados no Context.jsx
    
    * Final da criação de context e datepicker e timepicker

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,7 @@
-import { Button } from "../components";
 import { useAppContext } from "../Context";
 
 const Home: React.FC = () => {
-   const {user, changeLanguage} = useAppContext();
-   console.log(user);
-
-   return <Button onClick={changeLanguage}>MUDAR IDIOMA</Button>;
+   return 'Home';
 };
 
 export default Home;
--- src/views/Signin.tsx
+++ src/views/Signin.tsx
@@ -1,5 +1,29 @@
+import { Button, DatePicker, DateTimePicker } from "../components";
+import { useAppContext } from "../Context";
+
 const SignIn: React.FC = () => {
-   return 'SignIn'
+   const {showSnackMessage, showAlertMessage} = useAppContext();
+
+   return (
+      <>
+         <Button onClick={() => showSnackMessage("mensagem customizada")}>Clique aqui</Button>
+         <Button onClick={() => showAlertMessage("mensagem customizada" , "success", "filled")}>Clique sucess</Button>
+         <Button onClick={() => showAlertMessage("mensagem customizada" , "warning", "outlined")}>Clique warning</Button>
+         <Button onClick={() => showAlertMessage("mensagem customizada" , "error")}>Clique error</Button>
+         <Button onClick={() => showAlertMessage("mensagem customizada" , "info")}>Clique info</Button>
+
+         {/* O Horário exibido é sempre no formato UTC */}
+         <DateTimePicker
+            ampm={false}
+            format="DD/MM/YYYY HH:mm"
+            onChange={(value) => console.log(value.toString())}
+         />
+         <DatePicker
+            format="DD/MM/YYYY"
+            onChange={(value) => console.log(value.toString())}
+         />
+      </>
+   )
 };
 
 export default SignIn;
